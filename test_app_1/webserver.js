88f15abb138411265367e713731cfb54b62bd67a
Brian Mills <brianmills@millsconsulting.co.uk>
2015-12-07 12:42:07 +0000

    first version of test_app_1

---
+++ test_app_1/webserver.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+var app = connect();
+app.use(serveStatic("../test_app_1/public"));
+app.listen(5100);
+
+
