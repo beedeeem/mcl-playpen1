a988ca2bef6ff6abcd00856324e39944b006b7b2
bdmills <bdmills@192.168.0.8>
2015-12-14 17:16:51 +0000

    Exercise 2 complete

--- Exercise-02-02/js/editController.js
+++ Exercise-02-02/js/editController.js
@@ -8,11 +8,64 @@ angular.module('maintenance', [])
 
 function SiteEditCtrl($scope) {
     $scope.sites = sites;
+    $scope.startAdd = startAddFunc;
+    $scope.cancel=cancel;
+    $scope.add = add;
+    $scope.startEdit = startEdit;
+    $scope.save = save;
+    $scope.startRemove = startRemove;
+    $scope.remove = remove;
+    $scope.getSelected = getSelected;
+    
+    var selected = -1;
+    
     setView('list');
     
     function setView(view) {
+        if (view == 'list') {
+            selected = -1;
+        }
         $scope.view = view;
     } 
+    
+    function startAddFunc() {
+        $scope.siteBox='';
+        setView('add');
+    }
+    
+    function cancel() {
+        setView('list');
+    }
+    
+    function add() {
+        $scope.sites.push($scope.siteBox);
+        setView('list');
+    }
+    
+    function startEdit(index) {
+        selected = index;
+        $scope.siteBox = $scope.sites[index];
+        setView('edit');
+    }
+    
+    function save() {
+        $scope.sites[selected] = $scope.siteBox;
+        setView('list');
+    }
+    
+    function startRemove(index) {
+        selected = index;
+        setView('delete')
+    }
+    
+    function remove() {
+        $scope.sites.splice(selected, 1);
+        setView('list');
+    }
+    
+    function getSelected() {
+        return sites[selected];
+    }
 }
 
 
